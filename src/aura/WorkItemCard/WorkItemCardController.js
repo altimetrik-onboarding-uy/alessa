2b5c617355aea97f3b2166097957cbeaaed4da9d
Agustin Lessa <alessa@altimetrik.com>
2018-04-18 13:15:39 -0300

    WorkItemCard refactor and added record editing on the status board

---
+++ src/aura/WorkItemCard/WorkItemCardController.js
@@ -0,0 +1,27 @@
+({
+	doInit : function(component, event, helper) {
+		var item = component.get('v.workItem');
+		var iconName = '';
+		
+		switch(item.Type__c) {
+			case 'Idea':
+				iconName = 'standard:solution';
+				break;
+			case 'Action':
+				iconName = 'standard:connected_apps';
+				break;
+			case 'Fix':
+				iconName = 'standard:maintenance_plan';
+				break;
+		}
+
+		component.set('v.iconName', iconName);
+	},
+	openEdit: function(component, event, helper) {
+		var event = $A.get('e.force:editRecord');
+		event.setParams({
+			'recordId': component.get('v.workItem.Id')
+		});
+		event.fire();
+	}
+})
